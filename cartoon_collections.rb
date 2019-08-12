4f8319f7db3298ac6810ddcec6adb89cad58b406
Warren Loy Harper II <warrenharper2@att.net>
2019-08-12 14:25:49 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,6 +1,5 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
-end
+arr = ["Doc", "Dopey", "Bashful", "Grumpy"].join(",")
+puts arr.split(",")
 
 def summon_captain_planet# code an argument here
   # Your code here
